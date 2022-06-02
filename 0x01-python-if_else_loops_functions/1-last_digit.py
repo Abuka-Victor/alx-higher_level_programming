43070e80ce7483bf2a802906a49985fb434bd58f
Abuka-Victor <abukavictoro@gmail.com>
2022-06-02 02:26:12 +0100

    Rectified pycodestyle errors

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ lastNumber = int(str(number)[-1])
 lastNumber = lastNumber * -1 if number < 0 else lastNumber
 print("Last digit of", number, "is", lastNumber, end=" ")
 if lastNumber > 5:
-	print("and is greater than 5")
+    print("and is greater than 5")
 elif lastNumber == 0:
-	print("and is 0")
+    print("and is 0")
 elif lastNumber < 6 and lastNumber != 0:
-	print("and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-	if (i != 99):
-		print(f"{i:02d}", end=", ")
-	else:
-		print(f"{i}")
+    if (i != 99):
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{i}".format(i))
