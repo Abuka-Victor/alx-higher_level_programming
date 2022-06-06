4bd02a16bba2ed808120387e8dd0885f24583a4e
Victor Abuka <abukavictoro@gmail.com>
2022-06-06 22:19:52 +0000

    Adv 1

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,6 +9,21 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t listSize = PyList_Size(p);
-	printf("[*] Size of the Python List = %d", listSize);
+	int size, i, alloc;
+	PyObject *obj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *) p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+
+	}
 }
