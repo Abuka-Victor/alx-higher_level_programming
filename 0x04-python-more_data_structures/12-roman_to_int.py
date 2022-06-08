329daf378d48ab664a5002564e42714c541e420c
Abuka-Victor <abukavictoro@gmail.com>
2022-06-09 00:36:20 +0100

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string and type(roman_string) == str:
+        myDict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        result = 0
+        for i in roman_string:
+            result += myDict[i.upper()]
+
+        return result
+    return None
