f9f59ddae127f8e9bd17f07d6d33326a30c20d8d
Abuka-Victor <abukavictoro@gmail.com>
2022-06-09 01:24:54 +0100

    Task 12 fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    result = 0
     if roman_string and type(roman_string) == str:
         myDict = {"I": 1, "V": 5, "X": 10, "L": 50,
                   "C": 100, "D": 500, "M": 1000}
-        result = 0
-        for i in roman_string:
-            result += myDict[i.upper()]
-
-        return result
-    return 0
+        for i in range(len(roman_string)):
+            if i != (len(roman_string) - 1) and myDict[roman_string[i]] < myDict[roman_string[i + 1]]:
+                result += int(myDict[roman_string[i + 1]] - (myDict[roman_string[i + 1]] / 10))
+                result -= myDict[roman_string[i + 1]]
+            else:
+                result += myDict[roman_string[i]]
+    return result
