6dbcc6d629f7920616cdace587c4220c0c2413ac
Abuka-Victor <abukavictoro@gmail.com>
2022-06-21 23:44:33 +0100

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+A module containing the square class task 2
+"""
+
+
+class Square:
+    """
+    A square class for the alx project
+    """
+    def __init__(self, size=0):
+        """
+        Initialize the class
+        Args:
+            size: The size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
