aed115bd11f94a82c3556f685fcc654f69113741
Abuka-Victor <abukavictoro@gmail.com>
2022-06-21 23:37:05 +0100

    Added docs for task 0 and 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+A module containing the square class
+"""
 class Square:
+    """
+    A square class for the alx project
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+A module containing the square class task 1
+"""
 class Square:
+    """
+    A square class for the alx project
+    """
     def __init__(self, size):
+        """
+        Initialize the class
+        Args:
+            size: The size of the square
+        """
         self.__size = size
