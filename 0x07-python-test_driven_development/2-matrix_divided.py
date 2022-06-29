b84f8d32ec63371a1c644edce4fe95926ae24fc5
Abuka-Victor <abukavictoro@gmail.com>
2022-06-29 16:04:28 +0100

    Tasks

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+A module for a function that divides all elements of a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+
+    Args:
+        matrix:
+        div:
+
+    Returns:
+
+    """
+    for i in matrix:
+        if not all(isinstance(x, (int, float)) for x in i):
+            raise TypeError("matrix must be a matrix "
+                            "(list of lists) of integers/floats")
+
+    sizes = list(map(lambda x: len(x), matrix))
+    if (len(set(sizes))) != 1:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    result = list(map(lambda x:
+                      list(map(lambda y:
+                               round(y / div, 2), x)), matrix))
+    return result
--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-add_integer = __import__('0-add_integer').add_integer
+text_indentation = __import__('5-text_indentation').text_indentation
 
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
