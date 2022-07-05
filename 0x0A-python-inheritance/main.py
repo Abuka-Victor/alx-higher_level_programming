e1232314e8cb94debd5d2c57c127a81efd180469
Abuka-Victor <abukavictoro@gmail.com>
2022-07-05 16:50:20 +0100

    Task 7 test fix

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
-Square = __import__('11-square').Square
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-s = Square(13)
+bg = BaseGeometry()
 
-print(s)
-print(s.area())
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
 
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
