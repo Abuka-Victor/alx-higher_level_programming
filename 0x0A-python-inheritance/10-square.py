da967229e86a762a0a2695109e4110a15ba39a56
Abuka-Victor <abukavictoro@gmail.com>
2022-07-05 17:08:26 +0100

    Task 8-11 fix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python3
 """ Module for task 10 """
-rect = __import__("9-rectangle").Rectangle
+class BaseGeometry:
+    """ My base geometry class """
+
+    def area(self):
+        """ Calculates area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Integer Validator Method """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        return value
 
 
-class Square(rect):
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {width}/{height}".format(width=self.__width, height=self.__height)
+
+
+class Square(Rectangle):
     def __init__(self, size):
         self.__size = self.integer_validator("Size", size)
 
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 """ My module for task 9 """
 
-base = __import__('7-base_geometry').BaseGeometry
+
+class BaseGeometry:
+    """ My base geometry class """
+
+    def area(self):
+        """ Calculates area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Integer Validator Method """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        return value
 
 
-class Rectangle(base):
+class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         self.__width = self.integer_validator("width", width)
