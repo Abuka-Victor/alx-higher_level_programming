1d3d53318d16b164d784793c2b38c0bea8df0a7c
Abuka-Victor <abukavictoro@gmail.com>
2022-07-05 22:41:44 +0100

    Task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module for task 13 """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    lines = []
+    with open(filename, "r") as f:
+        for line in f:
+            lines.append(line)
+            if search_string in line:
+                lines.append(new_string)
+        
+    with open(filename, 'w') as f:
+        f.writelines(lines)
