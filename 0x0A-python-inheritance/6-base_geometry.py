90158eb090024e0416d7822016be44eab8489f03
Abuka-Victor <abukavictoro@gmail.com>
2022-07-05 10:15:38 +0100

    Task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Module for task 6 """
+
+
+class BaseGeometry:
+    """ My base geometry class """
+
+    def area(self):
+        """ Calculates area """
+        raise Exception("area() is not implemented")
