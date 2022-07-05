1229a8bb202e5640d0ae232cf797de41eb7057cb
Abuka-Victor <abukavictoro@gmail.com>
2022-07-05 10:02:43 +0100

    tasks 3 and 4

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+""" Module for task 3 """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Checks if it's kind of the same class """
+    return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+""" Module for task 4 """
+
+
+def inherits_from(obj, a_class):
+    """ is an instance of a class that inherits  """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
