9289f3279804626dbadfa2f7446f42f4715e38c6
Abuka-Victor <abukavictoro@gmail.com>
2022-07-05 11:34:48 +0100

    Task 9

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,3 +15,4 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+        return value
