389142464f3fe02032f27e609abd4b04730ca616
Abuka-Victor <abukavictoro@gmail.com>
2022-07-05 11:41:46 +0100

    Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Module for task 10 """
+rect = __import__("9-rectangle").Rectangle
+
+
+class Square(rect):
+    def __init__(self, size):
+        self.__size = self.integer_validator("Size", size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {size}/{size}".format(size=self.__size)
