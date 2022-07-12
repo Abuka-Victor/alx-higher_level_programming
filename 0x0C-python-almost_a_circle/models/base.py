7b86a1b8518d595b976efe44eb82c2842123b5d2
Abuka-Victor <abukavictoro@gmail.com>
2022-07-12 21:07:28 +0100

    task 15 fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Converts to a json string """
-        if list_dictionaries is not None or not list_dictionaries:
+        if list_dictionaries is not None or len(list_dictionaries) != 0:
             return json.dumps(list_dictionaries)
         return "[]"
 
