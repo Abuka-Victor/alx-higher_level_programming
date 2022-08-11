99209c0d887ad7742549c64b4fbe77c9865cd63c
Abuka-Victor <abukavictoro@gmail.com>
2022-08-11 17:24:55 +0100

    Task 9 - Fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- list cities in a db
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON states.id = cities.states_id 
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.states_id = states.id 
 ORDER BY cities.id ASC;
