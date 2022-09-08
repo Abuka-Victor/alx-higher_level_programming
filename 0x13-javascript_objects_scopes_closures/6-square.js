a72046b3514a074ed9e9f10220309de3c9cc4810
Abuka-Victor <abukavictoro@gmail.com>
2022-09-08 18:01:58 +0100

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,39 +1,8 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if ((w !== 0 && w > 0) && (h !== 0 && h > 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Rectangle = require('./5-square');
 
-  print () {
-    let symbol = 'X';
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 1; j < this.width; j++) {
-        symbol = symbol.concat('X');
-      }
-      console.log(symbol);
-      symbol = 'X';
-    }
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-}
-
-module.exports = class Square extends Rectangle {
-  constructor (s) {
-    super(s, s);
-  }
-
-  charPrint (c = 'X') {
+module.exports = class Squares extends Rectangle{
+   charPrint (c = 'X') {
     let symbol = c;
     for (let i = 0; i < this.height; i++) {
       for (let j = 1; j < this.width; j++) {
