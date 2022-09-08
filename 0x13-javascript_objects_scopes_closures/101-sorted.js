8dcdf5b7d766dcdbec8b5814ad38e8832c939e6a
Abuka-Victor <abukavictoro@gmail.com>
2022-09-08 21:16:21 +0100

    Task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const myDict = require('./101-data').dict;
 const newDict = {};
-for (let i in myDict){
-  if (newDict.hasOwnProperty(myDict[i])) {
+for (const i in myDict) {
+  if (myDict[i] in newDict) {
     newDict[myDict[i]].push(i);
   } else {
     newDict[myDict[i]] = [i];
