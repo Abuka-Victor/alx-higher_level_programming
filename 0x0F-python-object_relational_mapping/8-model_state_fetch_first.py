00eace3887fe721941e10e6535c63bbeafe133fa
Abuka Victor <abukavictoro@gmail.com>
2022-09-13 10:05:46 -0700

    Task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Script to print states via sqlalchemy
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+    result = engine.execute("SELECT * FROM states LIMIT 1")
+    if result:
+        for row in result:
+            print("{}: {}".format(row[0], row[1]))
+    else:
+        print("Nothing")
