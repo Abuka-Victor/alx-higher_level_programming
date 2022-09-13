d1ebc11b26c5e11f950d79f09c346c060e4aef2f
Abuka-Victor <abukavictoro@gmail.com>
2022-09-13 23:26:48 +0100

    Task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Script to print states via sqlalchemy
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    deleted = State.__table__.delete().where(State.id.ilike('%a%'))
+    session.execute(delete)
+    session.commit()
