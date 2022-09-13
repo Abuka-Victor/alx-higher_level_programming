d4be8249fdd0ba20d162d25b601e9ee0ddf62835
Abuka Victor <abukavictoro@gmail.com>
2022-09-13 09:46:41 -0700

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" A module to define the state table using SQLAlchemy"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
