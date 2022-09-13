ccdac2a9a778adc01130c27b4c780c37f93bb382
Abuka-Victor <abukavictoro@gmail.com>
2022-09-13 23:36:30 +0100

    Task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,5 +14,6 @@ if __name__ == "__main__":
         pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    deleted = session.query(State).filter(State.name.ilike('%a%')).delete()
+    deleted = session.query(State).filter(State.name.ilike('%a%'))
+    session.delete(deleted)
     session.commit()
