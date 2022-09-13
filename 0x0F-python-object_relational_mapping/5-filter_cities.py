e0909aef9c2d7177d5964ba123e454ba78d4e017
Abuka Victor <abukavictoro@gmail.com>
2022-09-13 05:37:47 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,14 +10,18 @@ if __name__ == "__main__":
                            charset="utf8")
     cur = conn.cursor()
     cur.execute("""SELECT City FROM (
-        SELECT cities.id AS ID, cities.name AS City, states.name AS State FROM cities
+        SELECT cities.id AS ID, cities.name AS City,
+        states.name AS State FROM cities
         JOIN states
         ON cities.state_id = states.id
-        ORDER BY cities.id ASC)
-	WHERE State=%s;
+        ORDER BY cities.id ASC) as Table1
+        WHERE State=%s;
         """, (sys.argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row != query_rows[-1]:
+            print("{}, ".format(*row), end="")
+        else:
+            print(*row)
     cur.close()
     conn.close()
