86b92a1b1fbc522c11cdfe4ee0ed816d7d2419b4
Abuka Victor <abukavictoro@gmail.com>
2022-09-13 09:49:00 -0700

    Task 6 fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """A class that maps to the states table"""
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
