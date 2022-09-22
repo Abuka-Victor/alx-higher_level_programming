6cf73f05876eacacc16de42311337c665d40e657
Abuka-Victor <abukavictoro@gmail.com>
2022-09-22 23:57:08 +0100

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # a script to check the size of the body of the response
-if (($# == 1)); then
-	curl -s $1 -w "%{size_download}\n" | tail -n 1
-fi
+curl -s $1 -w "%{size_download}\n" | tail -n 1
