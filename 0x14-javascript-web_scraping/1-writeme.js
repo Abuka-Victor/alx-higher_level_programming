ad30e2db051c9b826fcf220b0bd0853cf9f35895
Abuka-Victor <abukavictoro@gmail.com>
2022-10-19 00:32:59 +0100

    Task 1 semistandard fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,6 +3,5 @@ const fs = require('fs');
 const myargs = process.argv.slice(2);
 
 fs.writeFile(myargs[0], myargs[1], 'utf-8', (err) => {
-  if (err)
-    console.log(err);
+  if (err) { console.log(err); }
 });
